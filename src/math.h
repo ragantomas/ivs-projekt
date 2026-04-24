95b3b5cbe81e0abbb68d9bcce3663e6923d48bfe
Daniel Kratky <xkratkd00@stud.fit.vut.cz>
2026-04-24 13:52:19 +0200

    Created header file with declaration of used math functions

---
+++ src/math.h
@@ -0,0 +1,22 @@
+//======= Copyright (c) 2026, FIT VUT Brno, GNU GPL V3.0 ============//
+//
+// Purpose:     math library
+//
+// $NoKeywords: $ivs_project_2 $math.h
+// $Authors:    Daniel Kratky <xkratkd00@stud.fit.vut.cz>
+// $Date:       $2026-04-23
+//============================================================================//
+#ifndef MATH_H
+#define MATH_H
+
+/**
+ * @file math.h
+ * @author Daniel Kratky
+ *
+ * @brief Library for calculting math functions
+ *
+ * This file declares the global variables and function
+ * prototypes used in the math library.
+ */
+
+#endif // MATH_H
