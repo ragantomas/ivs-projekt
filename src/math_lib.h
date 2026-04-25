71126b1a98438a8df3f658b6e9bc107f97a67f32
xzakrad00-vut <xzakrad00@vutbr.cz>
2026-04-25 05:06:40 +0200

    Add files via upload

---
+++ src/math_lib.h
@@ -0,0 +1,22 @@
+//======= Copyright (c) 2026, FIT VUT Brno, GNU GPL V3.0 ============//
+//
+// Purpose:     Matematicka knihovna pro kalkulacku
+//
+// $NoKeywords: $ivs_project_2 $math_lib.h
+// $Authors:    Radek <xzakrad00@stud.fit.vut.cz>
+// $Date:       $2026-04-25
+//============================================================================//
+
+#ifndef MATH_LIB_H
+#define MATH_LIB_H
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double factorial(int n);
+double power(double base, double exponent);
+double root(double degree, double radicand);
+double logarithm(double x);
+
+#endif // MATH_LIB_H
