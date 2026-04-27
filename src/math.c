9b887ea536585efc80e7bf0e7028712b3c04d788
xzakrad00-vut <xzakrad00@vutbr.cz>
2026-04-27 21:30:31 +0200

    Update math.c

--- src/math.c
+++ src/math.c
@@ -3,12 +3,12 @@
 // Purpose:     math library
 //
 // $NoKeywords: $ivs_project_2 $math.c
-// $Authors:    Daniel Kratky <xkratkd00@stud.fit.vut.cz>
+// $Authors:    Radek Zak <xzakrad00@stud.fit.vut.cz>, Daniel Kratky <xkratkd00@stud.fit.vut.cz>
 // $Date:       $2026-04-24
 //============================================================================//
 /**
  * @file math.c
- * @author Daniel Kratky
+ * @author Daniel Kratky, Radek Zak
  *
  * @brief Library for calculting math functions
  */
@@ -17,12 +17,126 @@
 double add(double a, double b, int *error) {
     double result = a + b;
 
+    if(error){
     // overflow check (positive and negative)
     if (isinf(result)) {
         *error = 1; // overflow error
     } else {
         *error = 0;
     }
+    }
 
+    return result;
+}
+double sub(double a, double b, int *error) {
+    double result = a - b;
+
+    if(error){
+    // overflow check (positive and negative)
+    if (isinf(result)) {
+        *error = 1; // overflow error
+    } else {
+        *error = 0;
+    }
+    }
+
+    return result;
+}
+double mul(double a, double b, int *error) {
+    double result = a * b;
+
+    if(error){
+    // overflow check (positive and negative)
+    if (isinf(result)) {
+        *error = 1; // overflow error
+    } else {
+        *error = 0;
+    }
+    }
+
+    return result;
+}
+double divide(double a, double b, int *error) {
+    if (b == 0.0) {
+        if (error) {
+            *error = 1; // error: div by zero
+            }    
+        return 0.0;
+        } 
+        else {
+        if (error) {
+            *error = 0;
+            }
+    double result = a / b;
+    return result;
+    }
+}
+long long factorial(int n, int *error) {
+    if (n < 0){
+        if (error){
+            *error = 1;
+        }
+        return 0;
+    }
+    if (n == 0 || n == 1) {
+        if (error){
+            *error = 0;}
+        return 1;
+    }
+    //overflow check (21! doesn't fit long long)
+    if (n > 20) {
+        if (error){
+            *error = 1; // overflow error
+        }
+        return 0; 
+    }
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }   
+
+    if (error) {
+        *error = 0; 
+    }
+    return result; 
+}
+double power(double base, int exponent, int *error) {
+    if (exponent == 0) {
+        if (error) {
+            *error = 0;
+        }
+        return 1.0;
+    }
+    // error div by zero
+    if (base == 0.0 && exponent < 0) {
+        if (error){
+            *error = 1;
+        }
+        return 0.0;
+    }
+    
+    double result = 1.0;
+   
+    if (exponent > 0) {
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        
+    }
+    else {
+        for (int i = 0; i < -exponent; i++) {
+            result *= base;
+        }
+        
+        result = 1.0 / result;
+    }
+    if (error) {
+        if (isinf(result)) {
+            *error = 1;
+        } 
+        else {
+            *error = 0;
+        }
+    }
     return result;
 }
